06251e2bd6af022f02eee0b1d39e3207588e6cff
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2020-12-22 21:02:53 -0600

    Vista Ingresar

--- app/src/main/java/com/example/farmafast/MainActivity.java
+++ app/src/main/java/com/example/farmafast/MainActivity.java
@@ -1,17 +1,71 @@
 package com.example.farmafast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+import java.io.File;
+import java.io.IOException;
+import java.util.Calendar;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private EditText etCorreo, etContrasenia;
+    private Button btnIngresar, btnRegistroUsuario, btnRegistroRepartidor, btnRegistroEstablecimiento;
+    private TextView tvRestablecerContrasenia;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Componentes();
+    }
+
+    private void Componentes(){
+        etCorreo = findViewById(R.id.tietCorreoMain);
+        etContrasenia = findViewById(R.id.tietContraseniaMain);
+
+        btnIngresar = findViewById(R.id.bIngresarMain);
+        btnRegistroUsuario = findViewById(R.id.bRegistroUsuarioMain);
+        btnRegistroRepartidor = findViewById(R.id.bRegistroRepartidorMain);
+        btnRegistroEstablecimiento = findViewById(R.id.bRegistroEstablecimientoMain);
+
+        tvRestablecerContrasenia = findViewById(R.id.tvRestablecerContraseniaMain);
+
+        btnIngresar.setOnClickListener(this);
+        btnRegistroUsuario.setOnClickListener(this);
+        btnRegistroRepartidor.setOnClickListener(this);
+        btnRegistroEstablecimiento.setOnClickListener(this);
+        tvRestablecerContrasenia.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.bIngresarMain:
+                Toast.makeText(this, "bIngresarMain", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bRegistroUsuarioMain:
+                Toast.makeText(this, "bRegistroUsuarioMain", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bRegistroRepartidorMain:
+                Toast.makeText(this, "bRegistroRepartidorMain", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.bRegistroEstablecimientoMain:
+                Toast.makeText(this, "bRegistroEstablecimientoMain", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.tvRestablecerContraseniaMain:
+                Toast.makeText(this, "tvRestablecerContraseniaMain", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/example/farmafast/dbfirebase/Usuario.java
@@ -0,0 +1,74 @@
+package com.example.farmafast.dbfirebase;
+
+public class Usuario {
+    private String Uid;
+    private String Nombre;
+    private String ApellidoPaterno;
+    private String ApellidoMaterno;
+    private String Correo;
+    private String Contrasenia;
+    private String TipoUsuario;
+
+    public Usuario(){
+    }
+
+    public String getUid() {
+        return Uid;
+    }
+
+    public void setUid(String uid) {
+        Uid = uid;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return ApellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellido) {
+        ApellidoPaterno = apellido;
+    }
+
+    public String getApellidoMaterno() {
+        return ApellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        ApellidoMaterno = apellidoMaterno;
+    }
+
+    public String getCorreo() {
+        return Correo;
+    }
+
+    public void setCorreo(String correo) {
+        Correo = correo;
+    }
+
+    public String getContrasenia() {
+        return Contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        Contrasenia = contrasenia;
+    }
+
+    public String getTipoUsuario() {
+        return TipoUsuario;
+    }
+
+    public void setTipoUsuario(String tipoUsuario) {
+        TipoUsuario = tipoUsuario;
+    }
+
+    public String toString(){
+        return Nombre + " " + ApellidoPaterno + " " + ApellidoMaterno;
+    }
+}
