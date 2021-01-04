cbcc093d5ef35d7856abc0c143d83e215e3f9842
Orlando Gonzalez Albarran <ogonzaleza1@toluca.tecnm.mx>
2021-01-03 23:30:46 -0600

    Creditos

--- app/src/main/java/com/example/farmafast/ui/creditos/CreditosFragment.java
+++ app/src/main/java/com/example/farmafast/ui/creditos/CreditosFragment.java
@@ -23,11 +23,11 @@ public class CreditosFragment extends Fragment {
         creditosViewModel =
                 ViewModelProviders.of(this).get(CreditosViewModel.class);
         View root = inflater.inflate(R.layout.fragment_creditos, container, false);
-        final TextView textView = root.findViewById(R.id.text_gallery);
+        //final TextView textView = root.findViewById(R.id.text_gallery);
         creditosViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //textView.setText(s);
             }
         });
         return root;
